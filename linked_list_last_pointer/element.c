450c7b1be480fd3b8a547465bbc483623f3bb225
diastoff1 <pvictordias2003@gmail.com>
2024-03-08 17:48:32 -0300

    fc

--- linked_list_last_pointer/element.c
+++ linked_list_last_pointer/element.c
@@ -6,6 +6,6 @@ void element_print(Element element) {
     printf("%d", element);
 }
 
-int element_compare(Element element1, Element element2) {
-    return element1 - element2;
+int element_compare(Element element1, Element elementdois) {
+    return element1 - elementdois;
 }
